40ef0f9033475cbf616a7c9c232ebec5f0a035c0
dhanushgowdarl <rldhanush9606@gmail.com>
2023-12-15 13:16:31 +0530

    third commit

--- PetPal/src/exception/PetShelter.java
+++ PetPal/src/exception/PetShelter.java
@@ -17,4 +17,15 @@ class PetShelter {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
+
+	public void listAvailablePets() {
+		System.out.println("Available Pets:");
+		for (Pet pet : availablePets) {
+			try {
+				System.out.println("Name: " + pet.getName() + ", Age: " + pet.getAge() + ", Breed: " + pet.getBreed());
+			} catch (NullPointerException e) {
+				System.out.println("Error: Information missing for a pet.");
+			}
+		}
+	}
 }
