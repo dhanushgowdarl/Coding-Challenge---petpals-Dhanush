4d03f50ce7d0ed0474de80473192fad33352f340
dhanushgowdarl <rldhanush9606@gmail.com>
2023-12-15 13:46:06 +0530

    Update

--- PetPal/src/main/PetpalApplication.java
+++ PetPal/src/main/PetpalApplication.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import dao.IAdoptable;
 import entity.AdoptionEvent;
 import entity.Cats;
@@ -11,30 +13,49 @@ import exception.PetShelter;
 public class PetpalApplication {
 
 	public static void main(String[] args) {
-		Pet dog = new Dogs();
-		Pet cat = new Cats("Whiskers");
+		Scanner scanner = new Scanner(System.in);
+		PetShelter petShelter = new PetShelter(); // Initialize your PetShelter object
 
-		PetShelter shelter = new PetShelter();
-		shelter.addPet(dog);
-		shelter.addPet(cat);
+		int choice;
+		do {
+			System.out.println("=== PetPals Menu ===");
+			System.out.println("1. Display Pet Listings");
+			System.out.println("2. Record Cash Donation");
+			System.out.println("3. Manage Adoption Events");
+			System.out.println("0. Exit");
+			System.out.print("Enter your choice: ");
+			choice = scanner.nextInt();
 
-		shelter.listAvailablePets();
+			switch (choice) {
+			case 1:
+				displayPetListings();
+				break;
+			case 2:
+				recordCashDonation();
+				break;
+			case 3:
+				recordItemDonation();
+				break;
+			case 0:
+				System.out.println("Exiting PetPals. Goodbye!");
+				break;
+			default:
+				System.out.println("Invalid choice. Please enter a valid option.");
+			}
+		} while (choice != 0);
 
-		Donation cashDonation = new Donation("John Doe", 100.0, "2023-01-01");
-		Donation itemDonation = new Donation("Jane Doe", 50.0, "Toys");
-
-		cashDonation.recordDonation();
-		itemDonation.recordDonation();
-
-		AdoptionEvent adoptionEvent = new AdoptionEvent();
-
-		adoptionEvent.registerParticipant((IAdoptable) shelter);
-		adoptionEvent.registerParticipant((IAdoptable) dog);
-
-		adoptionEvent.hostEvent();
-
-		PetShelter shelter1 = new PetShelter();
-
-		shelter1.readPetsFromFile("pets.txt");
+		scanner.close();
 	}
+
+	private static void displayPetListings() {
+		System.out.println("Total Pets");
+	}
+
+	private static void recordCashDonation() {
+		System.out.println("Total Donation");
+	}
+
+	private static void recordItemDonation() {
+		       System.out.println("Total Items");
+	}    
 }
